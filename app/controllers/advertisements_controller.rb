925f06d469a717e5701474a69f7785ed548ba9f7
Rami Hamdan <hamdance0@gmail.com>
2017-08-28 20:38:23 +0200

    able to view index of adds and create new

--- app/controllers/advertisements_controller.rb
+++ app/controllers/advertisements_controller.rb
@@ -8,8 +8,22 @@ class AdvertisementsController < ApplicationController
   end
 
   def new
+    @advertisement = Advertisement.new
   end
 
   def create
+    @advertisement = Advertisement.new
+    @advertisement.title = params[:advertisement][:title]
+    @advertisement.copy = params[:advertisement][:copy]
+    @advertisement.price = params[:advertisement][:price]
+
+    if @advertisement.save
+      flash[:notice] = "Ad was saved."
+      redirect_to @advertisement
+
+    else
+      flash.now[:alert] = "There was an error saving your ad. Please try again."
+      render :new
+    end
   end
 end
--- spec/controllers/advertisements_controller_spec.rb
+++ spec/controllers/advertisements_controller_spec.rb
@@ -23,12 +23,22 @@ RSpec.describe AdvertisementsController, type: :controller do
   end
 end
 
-  # describe "GET #show" do
-  #   it "returns http success" do
-  #     get :show
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+  describe "GET #show" do
+    it "returns http success" do
+      get :show, {id :my_ad.id}
+      expect(response).to have_http_status(:success)
+    end
+
+    it "renders the #show view" do
+      get :show, {id: my_ad.id}
+      expect(response).to render_template :show
+    end
+
+    it "assigns my_ad to @ad" do
+      get :show, { id: my_ad.id}
+      expect(assigns(:advertisement)).to eq(my_ad)
+    end
+  end
   #
   # describe "GET #new" do
   #   it "returns http success" do
@@ -37,11 +47,25 @@ end
   #   end
   # end
   #
-  # describe "GET #create" do
-  #   it "returns http success" do
-  #     get :create
-  #     expect(response).to have_http_status(:success)
-  #   end
-  # end
+  describe "Advertisement create" do
+    it "returns http success" do
+      get :create
+      expect(response).to have_http_status(:success)
+    end
+
+    it "increases the number of advertisements by 1" do
+      expect{advertisement :create, advertisement: {title: RandomData.random_sentence, copy: RandomData.random_paragraph, price: 99}}.to change(Advertisement, :count).by(1)
+    end
+
+    it "assigns the new advertisement to @advertisement" do
+      post :create, advertisement: {title: RandomData.random_sentence, copy: RandomData.random_paragraph, price: 99}
+      expect(assigns(:advertisement)).to eq Advertisement.last
+    end
+
+    it "redirects to the new advertisement" do
+      advertisement :create, advertisement: {title: RandomData.random_sentence, copy: RandomData.random_paragraph, price: 99}
+      expect(response).to redirect_to Advertisement.last
+    end
+  end
 
 end
