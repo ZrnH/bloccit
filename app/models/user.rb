09ea7bd5a7888a1d3845d92f7acddd8715d6c2c0
Rami Hamdan <hamdance0@gmail.com>
2017-09-03 23:24:03 +0200

    validating capitalized names

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ApplicationRecord
   before_save { self.email = email.downcase if email.present?}
+  before_save :format_name
 
   validates :name, length: { minimum: 1, maximum: 100 }, presence: true
 
@@ -11,5 +12,13 @@ class User < ApplicationRecord
             uniqueness: { case_sensitive: false },
             length: { minimum: 3, maximum: 254 }
 
-    has_secure_password
+  has_secure_password
+  def format_name
+    names = []
+    name.split.each do |val|
+      names << val.capitalize
+    end
+    self.name = names.join(" ")
+  end
+
 end
