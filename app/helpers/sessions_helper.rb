f87e59222100ca214d81d422a4c182a2982fc5ba
Rami Hamdan <hamdance0@gmail.com>
2017-09-05 19:49:15 +0200

    gravatar working correctly

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -12,6 +12,7 @@ module SessionsHelper
   end
 
   def avatar_url(user)
-    gravatar_id = Digest::MD5::hexdigest(user.email).downcase"http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
+    gravatar_id = Digest::MD5::hexdigest(user.email).downcase
+    "http://gravatar.com/avatar/#{gravatar_id}.png?s=48"
   end
 end
