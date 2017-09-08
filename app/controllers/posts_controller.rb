1b6f304648a636bba89fb11dd85ddaf6ce703157
Rami Hamdan <hamdance0@gmail.com>
2017-09-08 14:50:32 +0200

    added roles with different permissions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -68,4 +68,12 @@ class PostsController < ApplicationController
     params.require(:post).permit(:title,:body)
   end
 
+  def authorize_user
+    post = Post.find(params[:id])
+
+    unless current_user == post.user || current_user.admin?
+      flash[:alert] = "You must be an admin to do that."
+      redirect_to [post.topic, post]
+    end
+  end
 end
