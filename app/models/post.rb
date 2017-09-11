0a47ecda13e99211ff8b351a41ea941f7074e203
Rami Hamdan <hamdance0@gmail.com>
2017-09-11 19:30:06 +0200

    send email on new posts

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,7 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
   after_create :create_vote
+  after_create :new_favorite
 
   has_many :votes, dependent: :destroy
   has_many :favorites, dependent: :destroy
@@ -30,6 +31,11 @@ class Post < ApplicationRecord
     age_in_days = (created_at - Time.new(1970,1,1)) / 1.day.seconds
     new_rank = points + age_in_days
     update_attribute(:rank, new_rank)
+  end
+
+  def new_favorite
+    Favorite.create(post: self, user: self.user)
+    FavoriteMailer.new_post(self).deilver_now
   end
 
   private
